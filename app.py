0e919c5ccd15a252208e23b9e78def4f0c5efd8d
Serena <sereprz@gmail.com>
2017-03-17 14:36:29 +0000

    prettify html

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index(content=None):
+def index(content=None, warning=False):
     if request.method == 'POST':
-        doc = Document(url=request.form['url'])
-        content = doc.summary()
-    return render_template('index.html', content=content)
+        if request.form['url']:
+            doc = Document(url=request.form['url'])
+            content = doc.summary()
+        else:
+            warning = True
+    return render_template('index.html', content=content, warning=str(warning))
 
 
 if __name__ == '__main__':
