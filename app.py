847845c4a0766c92ae588cb12cb0b31a4b1e1d9e
Serena <sereprz@gmail.com>
2017-03-31 12:56:46 +0100

    validate inputs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request
 
 from autosummary.document import Document
@@ -6,15 +7,22 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index(content=None, warning=False):
+def index(content=None, warning=None):
     if request.method == 'POST':
-        if request.form['url']:
-            content = Document(url=request.form['url']).summary()
-        elif request.form['raw']:
-            content = Document(text=request.form['raw']).summary()
+        if request.form['url'] and request.form['raw']:
+            warning = 'Please insert only one input.'
         else:
-            warning = True
-    return render_template('index.html', content=content, warning=str(warning))
+            if request.form['url']:
+                try:
+                    content = Document(url=request.form['url']).summary()
+                except requests.exceptions.MissingSchema:
+                    warning = 'Invalid URL. Did you mean http://{}?'.format(
+                        request.form['url'])
+            elif request.form['raw']:
+                content = Document(text=request.form['raw']).summary()
+            else:
+                warning = 'Missing input!'
+    return render_template('index.html', content=content, warning=warning)
 
 
 if __name__ == '__main__':
