e756266d0fe13617b28fa04965026ad6f7071d1e
Serena <sereprz@gmail.com>
2017-04-29 18:46:56 -0500

    add slider input

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ The final summary is built by ranking the sentences according to their final sco
 
 ### TODO
 - [x] text as input
-- [ ] input percentage of text to keep (slider)
+- [x] input percentage of text to keep (slider)
 - [ ] train word2vec from scratch
 - [ ] improve parser
 - [ ] code TextRank
--- app.py
+++ app.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index(content=None, warning=None):
     if request.method == 'POST':
+        ratio = float(request.form['slider'])/100
         if request.form['url'] and request.form['raw']:
             warning = 'Please insert only one input.'
         else:
             if request.form['url']:
                 try:
-                    content = Document(url=request.form['url']).summary()
+                    content = Document(url=request.form['url']).summary(ratio=ratio)
                 except requests.exceptions.RequestException as e:
                     warning = format(e)
             elif request.form['raw']:
-                content = Document(text=request.form['raw']).summary()
+                content = Document(text=request.form['raw']).summary(ratio=ratio)
             else:
                 warning = 'Missing input!'
     return render_template('index.html', content=content, warning=warning)
